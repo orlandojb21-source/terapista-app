8ef39f7c20866adf4872c3aea3e829e0d6982896
orlandojb21-source <orlandojb.21@gmail.com>
2026-05-29 15:23:05 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,31 +1,18 @@
-var CACHE_NAME = 'terapista-app-v2';
-var urlsToCache = ['/', '/index.html'];
+var CACHE_NAME = 'terapista-app-v3';
 
 self.addEventListener('install', function(event) {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
-  );
 });
 
 self.addEventListener('fetch', function(event) {
   if (event.request.url.includes('script.google.com')) return;
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
 
 self.addEventListener('activate', function(event) {
   event.waitUntil(
     caches.keys().then(function(keys) {
-      return Promise.all(
-        keys.filter(function(k) { return k !== CACHE_NAME; })
-            .map(function(k) { return caches.delete(k); })
-      );
+      return Promise.all(keys.map(function(k) { return caches.delete(k); }));
     })
   );
 });
